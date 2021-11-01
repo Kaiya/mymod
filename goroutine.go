bd3b6202edb22beab0173e32ee58655b43f89dee
kaiya <xiongkaiya@gmail.com>
2021-11-01 18:13:36 +0800

    init

--- goroutine.go
+++ goroutine.go
@@ -5,7 +5,6 @@ import (
 	"runtime"
 	"time"
 
-	"kaiyai.com/mymod/v2/goroutine"
 )
 
 // func sleep(s string) {
@@ -20,7 +19,5 @@ import (
 
 func main() {
 	fmt.Println(runtime.GOMAXPROCS(0))
-	go goroutine.Sleep("Hello")
-	go goroutine.Sleep("World")
 	time.Sleep(1000 * time.Second)
 }
